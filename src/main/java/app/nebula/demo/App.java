8d5a9d30724b8bf7a3ae5a76311fd7089353eabd
liyiting <liyiting@lizhi.fm>
2024-05-15 18:46:17 +0800

    使用springboot

--- src/main/java/app/nebula/demo/App.java
+++ src/main/java/app/nebula/demo/App.java
@@ -1,27 +1,22 @@
 package app.nebula.demo;
 
 import app.nebula.demo.api.GreetingsService;
-import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.bootstrap.DubboBootstrap;
 import org.apache.dubbo.config.bootstrap.builders.ApplicationBuilder;
 import org.apache.dubbo.config.bootstrap.builders.ProtocolBuilder;
 import org.apache.dubbo.config.bootstrap.builders.ServiceBuilder;
-
-import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+public class App
 {
     public static void main( String[] args )
     {
-        DubboBootstrap.getInstance()
-                .application(ApplicationBuilder.newBuilder().name("dubbo-samples-api").logger("slf4j").build())
-                .protocol(ProtocolBuilder.newBuilder().name("dubbo").port(50052).build())
-                .service(ServiceBuilder.newBuilder().interfaceClass(GreetingsService.class).ref(new GreetingsServiceImpl()).build())
-                .start()
-                .await();
+        SpringApplication.run(App.class, args);
     }
 }
